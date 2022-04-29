382e3e390a105de16ee402c7e7bd0db88cbf86dd
Dzhyhow_Dmitriy <dmytro.dzhyhov@stud.onu.edu.ua>
2022-04-29 20:06:14 +0300

    Add @Todo model class and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'todos/index'
+  get 'todos/create'
+  get 'todos/update'
+  get 'todos/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
