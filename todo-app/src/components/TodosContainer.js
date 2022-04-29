f765cc10748845f60e65e6d1473e681f5e2b3c05
Dzhyhow_Dmitriy <dmytro.dzhyhov@stud.onu.edu.ua>
2022-04-30 01:12:20 +0300

    Add simple fetch for todo-list

--- todo-app/src/components/TodosContainer.js
+++ todo-app/src/components/TodosContainer.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class TodosContainer extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			todos: []
+		}
+	}
+
+	getTodos() {
+		axios.get('/api/v1/todos')
+			.then(response => {
+				this.setState({ todos: response.data })
+			})
+			.catch(error => console.log(error))
+	}
+
+	componentDidMount() {
+		this.getTodos()
+	}
+
 	render() {
 		return (
 			<div>
 				<div>
 					<ul>
-						<li>Tasks will be here...</li>
+						{ this.state.todos.map((todo) => {
+							return (
+								<li todo={todo} key={todo.id}>
+									<input type="checkbox" />
+									<label>{todo.title}</label>
+									<span> (Delete) </span>
+								</li>
+							)
+						}) }
 					</ul>
 				</div>
 				<div>
