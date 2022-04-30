755ace8c1a269c113f3bbc8152ef890512ff1bf2
Dzhyhow_Dmitriy <dmytro.dzhyhov@stud.onu.edu.ua>
2022-04-30 13:04:12 +0300

    Add React authentication components

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,3 +1,4 @@
+/*
 import React, { Component } from 'react';
 import './App.css';
 // Импорт пользовательского компонента
@@ -14,4 +15,65 @@ function App() {
   );
 }
 
+export default App;
+*/
+
+import React, { Component } from 'react';
+import axios from 'axios'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Home from './components/Home';
+import Signup from './components/SignUp';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+      user: {}
+    };
+  };
+
+  handleLogin = (data) => {
+    this.setState({
+      isLoggedIn: true,
+      user: data.user
+    })
+  }
+  handleLogout = () => {
+    this.setState({
+      isLoggedIn: false,
+      user: {}
+    })
+  }
+
+  loginStatus = () => {
+    axios.get('http://localhost:3000/logged_in', { withCredentials: true })
+      .then(response => {
+        if (response.data.logged_in) {
+          this.handleLogin(response)
+        } else {
+          this.handleLogout()
+        }
+      })
+      .catch(error => console.log('api errors:', error))
+  };
+
+  componentDidMount() {
+    this.loginStatus()
+  }
+
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/' component={<Home/>} />
+            <Route exact path='/login' component={<Login/>} />
+            <Route exact path='/signup' component={<Signup/>} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
+};
 export default App;
