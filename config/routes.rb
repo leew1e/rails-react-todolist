0be00c2842c12b6b7d072ac07dc4253300d920f6
Dzhyhow_Dmitriy <dmytro.dzhyhov@stud.onu.edu.ua>
2022-04-30 12:13:39 +0300

    Add @SessionController and session routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  scope '/api/v1' do
+  scope "/api/v1" do
     resources :todos
   end
-  
+
+  post "/login", to: "sessions#create"
+  post "/logout", to: "sessions#destroy"
+  get "/logged_in", to: "sessions#is_logged_in?"
+
   resources :users, only: [:create, :show, :index]
 end
