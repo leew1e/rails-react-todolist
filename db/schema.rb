14f0a048d481498a7b5272ac3937ef9639a7d26b
Dzhyhow_Dmitriy <dmytro.dzhyhov@stud.onu.edu.ua>
2022-04-30 11:56:34 +0300

    Merge branch 'main' of https://github.com/leew1e/rails-react-todolist

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_29_170134) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_30_082811) do
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.boolean "done"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
